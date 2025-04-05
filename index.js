8e35c33973ec193c1b97eccfc2ea6ff0598a3f22
YaelAIcreations <yaelaicreations@gmail.com>
2025-04-05 17:12:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ConvergenceField from './ConvergenceField';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ConvergenceField />);
