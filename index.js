da4f6f850c0da380ee34d969ac9b05704fad3a7b
YaelAIcreations <yaelaicreations@gmail.com>
2025-04-05 17:10:12 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import ConvergenceField from './ConvergenceField';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ConvergenceField />);
